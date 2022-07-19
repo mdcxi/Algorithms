cd3dc25cd01ec7ca10a5905da062a55c108415ed
mdcxi <hannahkawaii1611@gmail.com>
2022-07-19 23:56:40 +0700

    Sorted Square Array

---
+++ Array/Sorted Square Array/SortedSquareArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class SortedSquareArray
+{
+    public int[] sortedSquareArray(int[] array)
+    {
+        int[] newArray = new int[array.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            newArray[i] = array[i] * array[i];
+        }
+        Array.Sort(newArray);
+        return newArray;
+    }
+}
