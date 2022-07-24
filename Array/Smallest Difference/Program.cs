4ae4fbc608aff16a46752208afbb2e30f8880b50
mdcxi <hannahkawaii1611@gmail.com>
2022-07-24 23:52:02 +0700

    Smallest Difference

---
+++ Array/Smallest Difference/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program {
+    public static int[] SmallestDifference(int[] arrayOne, int[] arrayTwo) {
+        // Write your code here.
+        Array.Sort(arrayOne);
+        Array.Sort(arrayTwo);
+        int idxOne = 0, idxTwo = 0;
+        int smallest = Int32.MaxValue;
+        int current = Int32.MaxValue;
+        int[] smallestPair = new int[2];
+
+        while (idxOne < arrayOne.Length
+               && idxTwo < arrayTwo.Length) {
+            int firstNum = arrayOne[idxOne];
+            int secondNum = arrayTwo[idxTwo];
+            
+            if (firstNum < secondNum) {
+                current = secondNum - firstNum;
+                idxOne++;
+            }
+            else if (firstNum > secondNum)
+            {
+                current = firstNum - secondNum;
+                idxTwo++;
+            }
+            else {
+                return new int[] { firstNum, secondNum };
+            }
+
+            if (smallest > current)
+            {
+                smallest = current;
+                smallestPair = new int[] { firstNum, secondNum };
+            }
+        }
+        return smallestPair;
+    }
+}
