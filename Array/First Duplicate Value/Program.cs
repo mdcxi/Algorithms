c5da5138f90979d37f4b04ba67a37f47e5b52ca9
mdcxi <hannahkawaii1611@gmail.com>
2022-07-25 23:57:05 +0700

    First Duplicate Value

---
+++ Array/First Duplicate Value/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class Program {
+    //O(n): where n is the length of the input array
+    public int FirstDuplicateValue(int[] array) {
+        // Write your code here.
+        HashSet<int> newArray = new HashSet<int>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (newArray.Contains(array[i]))
+            {
+                return array[i];
+            }
+            else
+            {
+                newArray.Add(array[i]);
+            }
+        }
+        return -1;
+    }
+}
