75d4b91766ecc68cfb06ee7beae7405f15c1eeda
mdcxi <hannahkawaii1611@gmail.com>
2022-07-25 23:43:36 +0700

    Monotonic  Array

---
+++ Array/Monotonic Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static bool IsMonotonic(int[] array)
+    {
+        // Write your code here.
+        var isIncreasing = false;
+        var isDecreasing = false;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i - 1] > array[i]) 
+                isDecreasing = true;
+            if (array[i - 1] < array[i]) 
+               isIncreasing = true;
+            if (isDecreasing && isIncreasing)
+                return false;
+        }
+
+        return true;
+    }
+}
