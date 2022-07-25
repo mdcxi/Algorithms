b4d5e73ac1403901ceea99f44a648baf464cfef1
mdcxi <hannahkawaii1611@gmail.com>
2022-07-25 23:25:44 +0700

    Spiral Traverse

---
+++ Array/Spiral Traverse/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program {
+    public static List<int> SpiralTraverse(int[,] array) {
+        // Write your code here.
+        if (array.GetLength((0)) == 0)
+            return new List<int>();
+        
+        var result = new List<int>();
+        var firstRow = 0;
+        var lastRow = array.GetLength(0) - 1;
+        var firstColumn = 0;
+        var lastColumn = array.GetLength(1) - 1;
+
+        while (firstColumn <= lastColumn && firstRow <= lastRow)
+        {
+            for (int column = firstColumn; column <= lastColumn; column++)
+            {
+                result.Add(array[firstRow, column]);
+            }
+
+            for (int row = firstRow + 1; row <= lastRow; row++)
+            {
+                result.Add(array[row, lastColumn]);
+            }
+
+            for (int column = lastColumn - 1; column >= firstColumn; column--)
+            {
+                if (firstRow == lastRow) break;
+                result.Add((array[lastRow, column]));
+            }
+            
+            for (int row = lastRow- 1; row > firstRow; row--)
+            {
+                if (firstColumn == lastColumn) break;
+                result.Add((array[row, firstColumn]));
+            }
+
+            firstRow++;
+            lastRow--;
+            firstColumn++;
+            lastColumn--;
+        }
+        return result;
+    }
+}
