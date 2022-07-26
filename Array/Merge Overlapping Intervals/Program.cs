20d7734458c683b42895671cb3a84cc96034b43a
mdcxi <hannahkawaii1611@gmail.com>
2022-07-26 17:20:46 +0700

    Merge Overlapping Intervals

---
+++ Array/Merge Overlapping Intervals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Program 
+{
+    //O(nlog(n)) time complexity
+    public int[][] MergeOverlappingIntervals(int[][] intervals) 
+    {
+        //in case of one interval, no merges possible
+        if (intervals.Length <= 1) return intervals;
+        
+        //sort the intervals by starting value
+        int[][] sortedIntervals = intervals.Clone() as int[][];
+        Array.Sort(sortedIntervals, (a,b) => a[0].CompareTo(b[0]));
+
+        List<int[]> mergedIntervals = new List<int[]>();
+        int[] currentInterval = sortedIntervals[0];
+        mergedIntervals.Add(currentInterval);
+
+        foreach (var nextInterval in sortedIntervals)
+        {
+            int currentIntervalEnd = currentInterval[1];
+            int nextIntervalStart = nextInterval[0];
+            int nextIntervalEnd = nextInterval[1];
+
+            if (currentIntervalEnd >= nextIntervalStart)
+            {
+                currentInterval[1] = Math.Max(currentIntervalEnd, nextIntervalEnd);
+            }
+            else
+            {
+                currentInterval = nextInterval;
+                mergedIntervals.Add(currentInterval);
+            }
+        }
+        return mergedIntervals.ToArray();
+    }
+}
