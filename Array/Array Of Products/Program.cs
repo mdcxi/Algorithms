4a973d51b98595734df4488bab7548513bc682f5
mdcxi <hannahkawaii1611@gmail.com>
2022-07-26 15:42:37 +0700

    Array of Products

---
+++ Array/Array Of Products/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program {
+    //O(n^2) time complexity
+    public int[] ArrayOfProducts(int[] array) {
+        // Write your code here.
+        int[] products = new int[array.Length];
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            int product = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (i != j)
+                {
+                    product *= array[j];
+                }
+
+                products[i] = product;
+            }
+        }
+        return products;
+    }
+}
