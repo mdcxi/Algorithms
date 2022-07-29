7c87365009da8d3df9a01450e52ec4c889a7c862
mdcxi <hannahkawaii1611@gmail.com>
2022-07-30 00:36:12 +0700

    Same BSTs

---
+++ Binary Search Trees/Same BSTs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program {
+    public static bool SameBsts(List<int> arrayOne, List<int> arrayTwo) {
+        // Write your code here.
+        if (arrayOne.Count != arrayTwo.Count) return false;
+        if (arrayOne.Count == 0 && arrayTwo.Count == 0) return true;
+        if (arrayOne[0] != arrayTwo[0]) return false;
+
+        List<int> leftOne = GetSmaller(arrayOne);
+        List<int> leftTwo = GetSmaller(arrayTwo);
+        List<int> rightOne = GetBiggerOrEqual(arrayOne);
+        List<int> rightTwo = GetBiggerOrEqual(arrayTwo);
+
+        return SameBsts(leftOne,leftTwo) && SameBsts(rightOne,rightTwo);
+    }
+    
+    public static List<int> GetSmaller(List<int> array)
+    {
+        List<int> smaller = new List<int>();
+
+        for (int i = 1; i < array.Count; i++)
+        {
+            if(array[i] <array[0]) smaller.Add(array[i]);
+        }
+
+        return smaller;
+    }
+
+    public static List<int> GetBiggerOrEqual(List<int> array)
+    {
+        List<int> biggerOrEqual = new List<int>();
+
+        for (int i = 1; i < array.Count; i++)
+        {
+            if(array[i] >= array[0]) biggerOrEqual.Add(array[i]);
+        }
+
+        return biggerOrEqual;
+    }
+}
