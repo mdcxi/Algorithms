6edc734458e8d852725d1db67022b9969d7e221d
mdcxi <hannahkawaii1611@gmail.com>
2022-07-30 01:24:51 +0700

    Validate Three Nodes

---
+++ Binary Search Trees/Validate Three Nodes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program {
+	// This is an input class. Do not edit.
+	public class BST {
+		public int value;
+		public BST left = null;
+		public BST right = null;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+
+	public bool ValidateThreeNodes(BST nodeOne, BST nodeTwo, BST nodeThree) {
+		// Write your code here.
+		if (isDescendant(nodeTwo, nodeOne)) return isDescendant(nodeThree, nodeTwo);
+		if (isDescendant(nodeTwo, nodeThree)) return isDescendant(nodeOne, nodeTwo);
+		return false;
+	}
+	
+	//whether the "target" is a descendant of the "node"
+	public bool isDescendant (BST node, BST target)
+	{
+		while (node !=null && node != target)
+		{
+			node = (target.value < node.value) ? node.left : node.right;
+		}
+
+		return node == target;
+	}
+}
