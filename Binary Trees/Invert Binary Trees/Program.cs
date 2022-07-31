0c6bdc92decf3a842db58268888e97293439517a
mdcxi <hannahkawaii1611@gmail.com>
2022-07-31 18:52:53 +0700

    Invert Binary Trees

---
+++ Binary Trees/Invert Binary Trees/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program {
+    public static void InvertBinaryTree(BinaryTree tree) {
+        // Write your code here.
+        if (tree == null) return;
+        SwapLeftAndRight(tree);
+        InvertBinaryTree(tree.left);
+        InvertBinaryTree(tree.right);
+    }
+
+    public static void SwapLeftAndRight(BinaryTree tree)
+    {
+        BinaryTree left = tree.left;
+        tree.left = tree.right;
+        tree.right = left;
+    }
+
+    public class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
