df74bfd7247c2d9ac67e6aab03ba7cf5dd1bddad
mdcxi <hannahkawaii1611@gmail.com>
2022-07-31 18:43:34 +0700

    Node Depths

---
+++ Binary Trees/Node Depths/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    private static int total;
+    public static int NodeDepths(BinaryTree root) {
+        // Write your code here.
+        total = 0;
+        NodeDepth(root, 0);
+        return total;
+    }
+
+    public static void NodeDepth(BinaryTree root, int depth)
+    {
+        total += depth;
+        if(root.left!=null)
+            NodeDepth(root.left, depth + 1);
+        if(root.right!=null)
+            NodeDepth(root.right, depth + 1);
+    }
+    public class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+}
