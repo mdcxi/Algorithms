0c6793b8a6af815bb432feae0e0678f838ebc4b8
mdcxi <hannahkawaii1611@gmail.com>
2022-08-02 15:37:01 +0700

    Math Path Sum In Binary Tree

---
+++ Binary Trees/Math Path Sum In Binary Tree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    private static int maxPath;
+    
+    public static int MaxPathSum(BinaryTree tree) {
+        // Write your code here.
+        maxPath = Int32.MinValue;
+        FindMaxSum(tree);
+        return maxPath;
+    }
+
+    public class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+
+    public static int FindMaxSum(BinaryTree tree)
+    {
+        if (tree == null) return 0;
+        int leftSum = Math.Max(FindMaxSum(tree.left), 0);
+        int rightSum = Math.Max(FindMaxSum(tree.right), 0);
+        maxPath = Math.Max(maxPath, tree.value + leftSum + rightSum);
+        return tree.value + Math.Max(leftSum, rightSum);
+    }
+}
