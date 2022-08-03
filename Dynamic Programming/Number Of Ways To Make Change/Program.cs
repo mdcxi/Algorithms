f54386c030ebf207c15bac00fd2c663e85b27e31
mdcxi <hannahkawaii1611@gmail.com>
2022-08-03 20:45:57 +0700

    Number Of Ways  To Make Change

---
+++ Dynamic Programming/Number Of Ways To Make Change/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program {
+    public static int NumberOfWaysToMakeChange(int n, int[] denoms) {
+        // Write your code here.
+        int[] ways = new int[n + 1];
+        ways[0] = 1;
+        foreach (int denomination in denoms)
+        {
+            for (int amount = 1; amount < n + 1; amount++)
+            {
+                if (denomination <= amount)
+                {
+                    ways[amount] += ways[amount - denomination];
+                }
+            }
+        }
+        return ways[n];
+    }
+}
