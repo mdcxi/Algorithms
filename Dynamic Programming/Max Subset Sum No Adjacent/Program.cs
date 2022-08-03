4f8d451050f76383e38f4c0164c3f4077d976380
mdcxi <hannahkawaii1611@gmail.com>
2022-08-03 19:35:52 +0700

    Max Subset Sum No Adjacent

---
+++ Dynamic Programming/Max Subset Sum No Adjacent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program {
+	public static int MaxSubsetSumNoAdjacent(int[] array) {
+		// Write your code here.
+		if (array.Length == 0) return 0;
+		if (array.Length == 1) return array[0];
+		if (array.Length == 2) return Math.Max(array[0], array[1]);
+		
+		int[] maxSums = new int[3];
+		maxSums[0] = array[0];
+		maxSums[1] = Math.Max(array[0], array[1]);
+
+		for (int i = 2; i < array.Length; i++)
+		{
+			maxSums[2] = Math.Max(array[i] + maxSums[0], maxSums[1]);
+			maxSums[0] = maxSums[1];
+			maxSums[1] = maxSums[2];
+		}
+		return maxSums[1];
+	}
+}
