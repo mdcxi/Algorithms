ec78ccbac2331bf54ac6f6bae5065d26367240fe
mdcxi <hannahkawaii1611@gmail.com>
2022-08-03 21:10:54 +0700

    Min Number Of Coins For Change

---
+++ Dynamic Programming/Min Number Of Coins For Change/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program {
+    public static int MinNumberOfCoinsForChange(int n, int[] denoms) {
+        // Write your code here.
+        int[] numberOfCoins = new int[n + 1];
+        Array.Fill(numberOfCoins,Int32.MaxValue);
+        numberOfCoins[0] = 0;
+        int toCompare = 0;
+        foreach (var denomination in denoms)
+        {
+            for (int amount = 0; amount < numberOfCoins.Length; amount++)
+            {
+                if (denomination <= amount)
+                {
+                    if (numberOfCoins[amount - denomination] == Int32.MaxValue)
+                    {
+                        toCompare = numberOfCoins[amount - denomination];
+                    }
+                    else
+                    {
+                        toCompare = numberOfCoins[amount - denomination] + 1;
+                    }
+
+                    numberOfCoins[amount] = Math.Min(numberOfCoins[amount], toCompare);
+                }
+            }
+        }
+        return numberOfCoins[n]!= Int32.MaxValue ? numberOfCoins[n] : -1;
+    }
+}
