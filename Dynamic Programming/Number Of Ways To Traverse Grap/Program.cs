50d5013728fe8464d76dcc0e188681f68bc0dcba
mdcxi <hannahkawaii1611@gmail.com>
2022-08-05 18:14:06 +0700

    Number Of Ways To Traverse Graph

---
+++ Dynamic Programming/Number Of Ways To Traverse Grap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program {
+
+	public int NumberOfWaysToTraverseGraph(int width, int height) {
+		// Write your code here.
+		if (width == 1 || height == 1)
+			return 1;
+		return NumberOfWaysToTraverseGraph(width-1,height) 
+		       + NumberOfWaysToTraverseGraph(width,height - 1);
+	}
+}
