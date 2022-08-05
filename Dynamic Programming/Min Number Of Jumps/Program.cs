443f2666ecfd182b1562980b168422e818380eda
mdcxi <hannahkawaii1611@gmail.com>
2022-08-05 21:06:56 +0700

    Min Number Of Jumps

---
+++ Dynamic Programming/Min Number Of Jumps/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program {
+    public static int MinNumberOfJumps(int[] array) {
+        // Write your code here.
+        int left = 0, right = 0, minJump = 0;
+        while (right < array.Length - 1)
+        {
+            int furthest = 0;
+            for (int i = left; i < right + 1; i++)
+            {
+                furthest = Math.Max(array[i] + i, furthest);
+            }
+
+            left = right + 1;
+            right = furthest;
+            minJump += 1;
+        }
+    
+        return minJump;
+    }
+}
